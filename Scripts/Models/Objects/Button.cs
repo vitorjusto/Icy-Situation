72121d36f585bcc8560edf56ef8937112e8e42df
vitorjusto <vitorhugojusto1@gmail.com>
2026-01-02 00:33:24 -0300

    create buttons sprites

--- Scripts/Models/Objects/Button.cs
+++ Scripts/Models/Objects/Button.cs
@@ -5,13 +5,23 @@ using System.Linq;
 namespace WinterGame.Scripts.Models.Objects;
 public partial class Button : Area2D
 {
+	public AnimatedSprite2D Sprite;
 	private readonly List<Node2D> _nodes = new();
 
+	public override void _Ready()
+		=> Sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		
 	public bool IsActived => _nodes.Any();
 
 	public void onBodyEntered(Node2D node)
-		=> _nodes.Add(node);
+	{
+		Sprite.Play("Pressing");
+		_nodes.Add(node);
+	}
 		
 	public void onBodyExited(Node2D node)
-		=> _nodes.Remove(node);
+	{
+		Sprite.Play("Idle");
+		_nodes.Remove(node);
+	}
 }
--- Scripts/Models/Objects/ResetButton.cs
+++ Scripts/Models/Objects/ResetButton.cs
@@ -3,4 +3,17 @@ using Godot;
 namespace WinterGame.Scripts.Models.Objects;
 
 public partial class ResetButton : Area2D
-{}
+{
+	public AnimatedSprite2D Sprite;
+
+	public override void _Ready()
+	{
+		Sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	public void OnBodyEntered(Node2D node)
+		=> Sprite.Play("Pressing");
+
+	public void OnBodyExited(Node2D node)
+		=> Sprite.Play("Idle");
+}
